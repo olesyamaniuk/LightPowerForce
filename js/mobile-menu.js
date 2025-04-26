b896e6183c161d17a2964f7ede739e2bcf6e8a67
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-04-26 15:16:56 +0300

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const navListRef = document.querySelector(".q-c-navigation-list");
-  const headerRef = document.querySelector(".q-c-header");
-  const navigationRef = document.querySelector(".q-c-navigation");
+  const navListRef = document.querySelector(".q-b-navigation-list");
+  const headerRef = document.querySelector(".q-b-header");
+  const navigationRef = document.querySelector(".q-b-navigation");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function toggleAnswer(element) {
   const answer = element.nextElementSibling;
-  const buttonImg = element.querySelector(".q-c-toggle-button img");
+  const buttonImg = element.querySelector(".q-b-toggle-button img");
 
   if (answer.style.display === "none" || answer.style.display === "") {
     answer.style.display = "block";
